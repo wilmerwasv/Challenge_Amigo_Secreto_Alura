1597f05b6c8fffabfe4133537ba2d9e105ab2568
WilWasv <37233882+wilmerwasv@users.noreply.github.com>
2025-02-25 14:22:11 -0300

    se crear funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let  amigos = [] ;
+
+almacenar_en_lista (nombre) { 
+if (nombre === "") {
+alert ("no se admiten campos vacios");
+    }else{
+amigos.push(nombre)
+ }
+ }
