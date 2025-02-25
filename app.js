8623e38f13aa6ca6ba2ad3290b15f83c2677ef09
WilWasv <37233882+wilmerwasv@users.noreply.github.com>
2025-02-25 14:27:38 -0300

    ok

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let  amigos = [] ;
 
-almacenar_en_lista (nombre) { 
+agregarAmigo(nombre) { 
 if (nombre === "") {
 alert ("no se admiten campos vacios");
     }else{
